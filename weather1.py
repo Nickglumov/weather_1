c789383faa102faa642f98f3e4272ef9221197e1
Nickglumov <nekitglum@gmail.com>
2025-04-24 20:55:16 +0200

    Update weather1.py

--- weather1.py
+++ weather1.py
@@ -1,7 +1,7 @@
 import requests
 
+
 def get_weather(city):
-    base_url = "http://wttr.in/"
     params = {
         'm': '',
         'n': '',
@@ -9,18 +9,19 @@ def get_weather(city):
         'T': '',
         'lang': 'ru'
     }
-    
-    try:
-        response = requests.get(f"http://wttr.in/{city}", params=params)
-        response.raise_for_status()
-        return response.text
+
+    response = requests.get(f"http://wttr.in/{city}", params=params)
+    response.raise_for_status()
+    return response.text
+
 
 def main():
     cities = ["Лондон", "Шереметьево", "Череповец"]
-    
+
     for city in cities:
         weather = get_weather(city)
         print(weather)
+
 
 if __name__ == '__main__':
     main()
